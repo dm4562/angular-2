d0d9fc00233fc7d20aae8915fa5e906a9fa6cf3b
Dhruv Mehra <dmehra6@gatech.edu>
2016-07-25 11:14:34 -0400

    finish the heroes tutorail

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit, OnDestroy } from '@angular/core';
 import { Hero } from './hero';
 import { ActivatedRoute } from '@angular/router';
 import { HeroService } from './hero.service';
@@ -11,8 +11,11 @@ import { HeroService } from './hero.service';
 
 
 export class HeroDetailComponent implements OnInit, OnDestroy {
-  hero: Hero;
+  @Input() hero: Hero;
+  @Output() close = new EventEmitter();
+  error: any;
   sub: any;
+  navigated: boolean = false;
 
   constructor(
     private heroService: HeroService,
@@ -21,10 +24,15 @@ export class HeroDetailComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.sub = this.route.params.subscribe((params) => {
-      let id = +params['id'];
-      this.heroService.getHero(id).then(
-        (hero) => this.hero = hero
-      );
+      if (params['id'] !== undefined) {
+        let id = +params['id'];
+        this.navigated = true;
+        this.heroService.getHero(id).then(
+          (hero) => this.hero = hero);
+      } else {
+        this.navigated = false;
+        this.hero = new Hero();
+      }
     });
   }
 
@@ -32,8 +40,21 @@ export class HeroDetailComponent implements OnInit, OnDestroy {
     this.sub.unsubscribe();
   }
 
-  goBack() {
-    window.history.back();
+  save() {
+    this.heroService.save(this.hero).then(
+      (hero) => {
+        this.hero = hero;
+        this.goBack(hero);
+      }).catch(
+      (error) => this.error = error
+      );
+  }
+
+  goBack(savedHero: Hero = null) {
+    this.close.emit(savedHero);
+    if (this.navigated) {
+      window.history.back();
+    }
   }
 
 };
